ddd102c699b7ea2741bce8fa4b1421c1f7f13ff2
zcodlab <129913832+zcodlab@users.noreply.github.com>
2025-05-29 23:59:03 +0000

    251.uni.fp.pd4.Ejercicio8

--- 251.uni.fp.pd4.Ejercicio8/main.cpp
+++ 251.uni.fp.pd4.Ejercicio8/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cmath>
+#include <string>
 using namespace std;
 
 struct coordenada{
@@ -13,31 +15,55 @@ struct arista{
     float distancia;
     string nombre;
 };
-
+void InicializarArista(arista *A,string np1,string np2,float x1, float y1, float x2,float y2 ){
+    A->P1.nombre=np1;
+    A->P2.nombre=np2;
+    A->P1._X=x1;
+    A->P1._Y=y1;
+    A->P2._X=x2;
+    A->P2._Y=y2;
+    A->nombre=A->P1.nombre+"-"+A->P2.nombre;
+    A->distancia=sqrt(pow((x2-x1),2)+pow((y2-y1),2));
+}
+float calcularArea(arista a,arista b,arista c){
+    //aplicando el metodo de heron
+    float s=(a.distancia+b.distancia+c.distancia)/2.0;
+    float area=sqrt(s*(s-a.distancia)*(s-b.distancia)*(s-c.distancia));
+    return area;
+}
+void mostrarArista(arista A){
+    cout<<"------------------------------------------------------------"<<endl;
+    cout<<"Nombre de arista:"<<A.nombre<<endl;
+    cout<<"Coordenadas del punto:"<<A.P1.nombre<<": ("<<A.P1._X<<","<<A.P1._Y<<")"<<endl;
+    cout<<"Coordenadas del punto:"<<A.P2.nombre<<": ("<<A.P2._X<<","<<A.P2._Y<<")"<<endl;
+    cout<<"Distancia de la arista: "<<A.distancia<<endl;
+}
 
 int main()
 {  string A,B,C;//triangulo con 3 vertices
     float x1,x2,x3,y1,y2,y3,area;
-    arista D1,D2,D3//triangulo con 3 aristas
+    arista D1,D2,D3;//triangulo con 3 aristas
     //ingreso de datos
-    cin.ignore();
-    cout << "Ingrese el nombre del primer vertice" << endl;
+    cout << "Ingrese el nombre del primer vertice: ";
     getline(cin,A);
-    cout << "Ingrese la coordenada x del primer vertice" << endl;
+    cin.ignore();
+    cout << "Ingrese la coordenada x del primer vertice: " << endl;
     cin>>x1;
-    cout << "Ingrese la coordenada y del primer vertice" << endl;
+    cout << "Ingrese la coordenada y del primer vertice: " << endl;
     cin>>y1;
-    cout << "Ingrese el nombre del segundo vertice" << endl;
+    cin.ignore();
+    cout << "Ingrese el nombre del segundo vertice: " << endl;
     getline(cin,B);
-    cout << "Ingrese la coordenada x del segundo vertice" << endl;
+    cout << "Ingrese la coordenada x del segundo vertice: " << endl;
     cin>>x2;
-    cout << "Ingrese la coordenada y del segundo vertice" << endl;
+    cout << "Ingrese la coordenada y del segundo vertice: " << endl;
     cin>>y2;
-    cout << "Ingrese el nombre del tercer vertice" << endl;
+    cin.ignore();
+    cout << "Ingrese el nombre del tercer vertice: " << endl;
     getline(cin,C);
-    cout << "Ingrese la coordenada x del tercer vertice" << endl;
+    cout << "Ingrese la coordenada x del tercer vertice: " << endl;
     cin>>x3;
-    cout << "Ingrese la coordenada y del tercer vertice" << endl;
+    cout << "Ingrese la coordenada y del tercer vertice: " << endl;
     cin>>y3;
     //inicializando
     InicializarArista(&D1,A,B,x1,y1,x2,y2);
